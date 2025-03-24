9125d279b5746e11c3d11be4a30fd29904a6611c
Ansel <anselanselmi@gmail.com>
2025-03-24 13:46:25 -0600

    Testing Cog Memory with o7

--- checkpoint_manager.py
+++ checkpoint_manager.py
@@ -41,7 +41,7 @@ class CheckpointManager:
         """
         self.data["processed"] = list(processed_set)
         if timings:
-            self.data["times"].update(timings)
+            self.data["times"].update_memory(timings)
         try:
             with open(self.filepath, "w", encoding="utf-8") as f:
                 json.dump(self.data, f, indent=2)
